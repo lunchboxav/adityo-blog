fac2360dc6861a0632538b6c80287dc3dcc1fac6
Adityo Pratomo <adityo@tetrate.io>
2019-12-30 14:30:22 +0700

    added google analytics
    Signed-off-by: Adityo Pratomo <adityo@tetrate.io>

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,20 @@ module.exports = {
 				name: `pages`,
 				path: `${__dirname}/src/pages`
 			}
-		}
+		},
+		{
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-38323773-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10
+      },
+    },
 	] 
 }
